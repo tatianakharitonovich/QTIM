631fff9f900d048ffbcb5b19747cc3f99303d9e4
Tanya Kharitonovich <k.tanya@tut.by>
2024-04-18 12:04:53 +0200

    feat: add `header`

---
+++ components/header.vue
@@ -0,0 +1,89 @@
+<template>
+  <header class="header">
+    <div class="header-wrap">
+      <Logo />
+      <div class="header-content">
+        <nav class="header-links">
+          <NuxtLink
+            v-for="link in links"
+            :key="link.title"
+            :to="link.path"
+            class="header-link"
+          >
+            {{ link.title }}
+          </NuxtLink>
+        </nav>
+        <div class="header-buttons">
+          <LanguageToggle />
+          <button class="header-button">
+            Let’s work
+          </button>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+const links: HeaderLinks[] = [
+  {
+    path: '/works',
+    title: 'Works',
+  },
+  {
+    path: '/about',
+    title: 'About',
+  },
+]
+</script>
+
+<style lang="scss" scoped>
+.header {
+  width: 100%;
+  max-width: 87rem;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 2.125rem 1rem;
+
+  &-wrap {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  &-content {
+    display: flex;
+    justify-content: space-between;
+    gap: 5.625rem;
+  }
+
+  &-links {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 3.625rem;
+  }
+
+  &-link {
+    font-size: 1.25rem;
+    line-height: 1;
+  }
+
+  &-buttons {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 1rem;
+
+  }
+
+  &-button {
+    padding: 1rem 2rem;
+    color: var(--main-bg-color);
+    font-size: 1.25rem;
+    line-height: 1;
+    background-color: var(--main-color);
+    border-radius: 1.625rem;
+  }
+}
+</style>
