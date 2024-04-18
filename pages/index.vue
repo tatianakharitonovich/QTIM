504efcf177afb8c00722a9795ace33f5e8ae1a74
Tanya Kharitonovich <k.tanya@tut.by>
2024-04-18 12:04:31 +0200

    feat: add index page

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <div v-if="pending">
+      <h3>Loarding...</h3>
+    </div>
+    <div v-else-if="error">
+      <h3>Error code {{ error.statusCode }}</h3>
+      <h3>Error message {{ error.message }}</h3>
+    </div>
+    <div v-else-if="posts">
+      <PostsList :list="posts" />
+    </div>
+    <div v-else>
+      <h3>There is no data</h3>      
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const {pending, data: posts, error } = await useFetch<IPost[]>(
+  'https://6082e3545dbd2c001757abf5.mockapi.io/qtim-test-work/posts/',
+  {
+    lazy: false,
+    server:false,
+  }
+)
+</script>
