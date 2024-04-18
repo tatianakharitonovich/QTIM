2df6e07c54769bf90a348a95b0849540b7908659
Tanya Kharitonovich <k.tanya@tut.by>
2024-04-18 12:05:35 +0200

    feat: add layout

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="wrap">
+    <Header />
+    <main>
+      <slot />
+    </main>    
+    <Footer />
+  </div>  
+</template>
+
+<style lang="scss" scoped>
+.wrap {
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 100vh;
+}
+</style>
